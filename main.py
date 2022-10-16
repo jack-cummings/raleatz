59fdca235a9e0a7a15131607676141a94e741f50
jack-cummings <jmcummi2@ncsu.edu>
2022-10-16 18:07:30 -0400

    user input form started

--- main.py
+++ main.py
@@ -12,19 +12,40 @@ import uvicorn
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="assets"), name="assets")
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
 
 templates = Jinja2Templates(directory="templates/webapp")
 
 
 @app.get("/")
 async def home(request: Request):
-    #try:
-    return templates.TemplateResponse('index.html', {"request": request})
+    try:
+        return templates.TemplateResponse('index.html', {"request": request})
 
-    # except Exception as e:
-    #     print(e)
-    #     return templates.TemplateResponse('v2_error.html', {"request": request})
+    except Exception as e:
+        print(e)
+        return templates.TemplateResponse('error.html', {"request": request})
+
+@app.get("/input")
+async def user_input(request: Request):
+    try:
+        return templates.TemplateResponse('input.html', {"request": request})
+
+    except Exception as e:
+        print(e)
+        return templates.TemplateResponse('error.html', {"request": request})
+
+@app.post("/save_input")
+async def save_input(request: Request):
+    # Collect User Input
+    body = await request.body()
+    inputPC = body.decode('UTF-8').split('=')[1].strip()
+    try:
+        return templates.TemplateResponse('input.html', {"request": request})
+
+    except Exception as e:
+        print(e)
+        return templates.TemplateResponse('error.html', {"request": request})
 
 
 if __name__ == '__main__':
